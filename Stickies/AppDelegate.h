ce19fb90e9e8f65f1d63428cacc17f08739e3ae4
whynoceros <whynoceros@gmail.com>
2015-06-02 14:18:47 -0700

    Initial Commit

---
+++ Stickies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stickies
+//
+//  Created by Gabe's Laptop on 6/2/15.
+//  Copyright (c) 2015 whynoceros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stickies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stickies
+//
+//  Created by Gabe's Laptop on 6/2/15.
+//  Copyright (c) 2015 whynoceros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
